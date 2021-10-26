3f2a1132fb634c7b2cf1ff7a48710b7c5ee3a1bd
“Nikka” <njacuna@spectrumone.co>
2021-10-26 10:30:54 +0800

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjqU_EPwrmqMcpJQFOp_XkHVzaCW2JjD0",
+  authDomain: "fir-auth-c89a7.firebaseapp.com",
+  projectId: "fir-auth-c89a7",
+  storageBucket: "fir-auth-c89a7.appspot.com",
+  messagingSenderId: "785591531718",
+  appId: "1:785591531718:web:5dcf0490cb0a2f447e3fc7",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
